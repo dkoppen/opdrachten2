ea6d444e99b52da93a0e6ab4ae0f46afb5bf0ab3
Dennis <dkoppen@roc-dev.com>
2016-09-09 13:58:39 +0200

    gaat prima

---
+++ src/h042/Flag.java
@@ -0,0 +1,58 @@
+package h042;
+
+import java.applet.Applet;
+import java.awt.*;
+
+//<applet code=Flag.class height=400 width=400></applet>
+
+
+public class Flag extends Applet
+
+{
+
+    public void paint(Graphics g)
+
+    {
+
+        Color c;
+
+        c=Color.black;
+
+        g.setColor(c);
+
+        g.fillRect(237,114,10,500);
+
+        c=Color.red;
+
+        g.setColor(c);
+
+        g.fillRect(248,125,200,25);
+
+        c=Color.white;
+
+        g.setColor(c);
+
+        g.fillRect(248,150,200,25);
+
+        c=Color.blue;
+
+        g.setColor(c);
+
+        g.fillRect(248,175,200,25);
+
+        c=Color.blue;
+
+        g.setColor(c);
+        
+        int l=0;
+        int x=355,y=162;
+        double x1,y1;
+        double d;
+
+        c=Color.black;
+
+
+
+    }
+
+}
